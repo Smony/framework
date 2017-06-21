d13d3e108f9b64acdce99e5235a99cb059dfbf36
Smony <dev.symonchuk@gmail.com>
2017-06-21 15:06:08 +0300

    testing RedBeanPHP

--- tests/index.php
+++ tests/index.php
@@ -6,6 +6,8 @@
 
 	//R::setup( 'mysql:host=localhost;dbname=mydatabase', 'myusername', 'mypassword' );
 	R::setup($db['dsn'], $db['user'], $db['pass']);
+	R::freeze( TRUE ); //не изменять размер колонок
+	R::fancyDebug( TRUE ); //включить отладчик on-true off-false
 /* 
 	if(R::testConnection() == true)
 	{
@@ -16,14 +18,36 @@
 		echo "Проверьте соединение с базой!";
 	} */
 	
-	//Create tets table Category
-	$category = R::dispense('category');
+	//CREATE
+	 // $category = R::dispense('category');
 	
-	$category->title = 'Name';
-	$category->text = 'Text';
+	 // $category->title = 'new title5';
+	 // $category->text = 'new text5';
 	
-	$res = R::store($category);
-	// $category = R::load('category', 9);
+	 // $res = R::store($category);
+	 
 	
-
+	//READ
+/* 	$res = R::load('category', 27);
+	echo $res->title;
+	echo $res['title']; */
+	
+	 // UPDATE
+	 // $cat_name = R::load('category', 6);
+	 // $cat_name->title = 'new title6';
+	 // $cat_name->text = 'new text6';
+	 // R::store($cat_name);
+	 // echo $cat_name->title .'<br>';  
+	
+	// DELETE
+	// $delete = R::load('category', 26);
+	// R::trash($delete); 
+    // R::wipe( 'category' ); 
+	
+	// $res = R::findAll('category', 'id > ?', [5]);
+	// $res = R::findAll('category', 'title LIKE ?', ['%cat%']);
+	// $res = R::findOne('category', 'id = 2');
+	// $res = R::findOne('category', 'id = 2');
+	// dd($res);
+	
 	
