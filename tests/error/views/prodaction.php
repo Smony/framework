c7dab0a8a343598fccd2d2cb72e78e3a5d108a85
Smony <dev.symonchuk@gmail.com>
2017-06-23 16:19:19 +0300

    Added testing error Handler and logs

---
+++ tests/error/views/prodaction.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="header clearfix">
+        <h3 class="text-muted">Error</h3>
+    </div>
+    <div class="row">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading">Error Handler</div>
+            <div class="panel-body">
+                <p>Произошла ошибка</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
